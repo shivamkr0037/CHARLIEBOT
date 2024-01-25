74c075975de5d150387641571f33e3f6a9a662f4
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-25 21:19:18 +0530

    Update time_format.py

--- Dxbots/utils/time_format.py
+++ Dxbots/utils/time_format.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2024 DX-MODS
+#Licensed under the  MIT License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
 def get_readable_time(seconds: int) -> str:
     count = 0
     readable_time = ""
