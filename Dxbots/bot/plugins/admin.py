3390bfcf7818a693c9238f727d3e0dda58b5e868
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-25 23:31:09 +0530

    Update admin.py

--- Dxbots/bot/plugins/admin.py
+++ Dxbots/bot/plugins/admin.py
@@ -4,155 +4,180 @@
 #Author ZIYAN
 #if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
 
-from datetime import datetime
-from pytz import timezone
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
-from config import Config
-from aiohttp import web
-from route import web_server
-from helper.keepalive import ping_server
-import asyncio
-import logging
-import logging.config
-
-# Get logging configurations
-logging.config.fileConfig("logging.conf")
-logging.getLogger().setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-logger = logging.getLogger(__name__)
-
-
-class Bot(Client):
-
-    def __init__(self):
-        super().__init__(
-            name="Telegraph-Bot",
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            bot_token=Config.BOT_TOKEN,
-            workers=200,
-            plugins={"root": "plugins"},
-            sleep_threshold=15,
-        )
-
-    async def start(self):
-        await super().start()
-        me = await self.get_me()
-        self.mention = me.mention
-        self.username = me.username  
-        self.uptime = Config.BOT_UPTIME     
-        if Config.WEBHOOK:
-            asyncio.create_task(ping_server())
-            app = web.AppRunner(await web_server())
-            await app.setup()
-            bind_address = "0.0.0.0"
-            await web.TCPSite(app, bind_address, Config.PORT).start()     
-        print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
-        for id in Config.ADMIN:
-            try: await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
-            except: pass
-        if Config.LOG_CHANNEL:
-            try:
-                curr = datetime.now(timezone("Asia/Kolkata"))
-                date = curr.strftime('%d %B, %Y')
-                time = curr.strftime('%I:%M:%S %p')
-                await self.send_message(Config.LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")                                
-            except:
-                print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
-
-    async def stop(self, *args):
-        await super().stop()      
-        print("Bᴏᴛ Sᴛᴏᴩᴩᴇᴅ.. Byᴇ")
-       
-
-Bot().run()
 import os
+
 import time
+
 import string
+
 import random
+
 import asyncio
+
 import aiofiles
+
 import datetime
+
 from Dxbots.utils.broadcast_helper import send_msg
+
 from Dxbots.utils.database import Database
+
 from Dxbots.bot import DxStreamBot
+
 from Dxbots.vars import Var
+
 from pyrogram import filters, Client
+
 from pyrogram.types import Message
+
 db = Database(Var.DATABASE_URL, Var.name)
+
 Broadcast_IDs = {}
 
+
+
 @DxStreamBot.on_message(filters.command("users") & filters.private )
+
 async def sts(c: Client, m: Message):
+
     user_id=m.from_user.id
+
     if user_id in Var.OWNER_ID:
+
         total_users = await db.total_users_count()
+
         await m.reply_text(text=f"Total Users in DB: {total_users}", quote=True)
+
         
+
         
+
 @DxStreamBot.on_message(filters.command("broadcast") & filters.private  & filters.user(list(Var.OWNER_ID)))
+
 async def broadcast_(c, m):
+
     user_id=m.from_user.id
+
     out = await m.reply_text(
+
             text=f"Broadcast initiated! You will be notified with log file when all the users are notified."
+
     )
+
     all_users = await db.get_all_users()
+
     broadcast_msg = m.reply_to_message
+
     while True:
+
         broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+
         if not Broadcast_IDs.get(broadcast_id):
+
             break
+
     start_time = time.time()
+
     total_users = await db.total_users_count()
+
     done = 0
+
     failed = 0
+
     success = 0
+
     Broadcast_IDs[broadcast_id] = dict(
+
         total=total_users,
+
         current=done,
+
         failed=failed,
+
         success=success
+
     )
+
     async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
+
         async for user in all_users:
+
             sts, msg = await send_msg(
+
                 user_id=int(user['id']),
+
                 message=broadcast_msg
+
             )
+
             if msg is not None:
+
                 await broadcast_log_file.write(msg)
+
             if sts == 200:
+
                 success += 1
+
             else:
+
                 failed += 1
+
             if sts == 400:
+
                 await db.delete_user(user['id'])
+
             done += 1
+
             if Broadcast_IDs.get(broadcast_id) is None:
+
                 break
+
             else:
+
                 Broadcast_IDs[broadcast_id].update(
+
                     dict(
+
                         current=done,
+
                         failed=failed,
+
                         success=success
+
                     )
+
                 )
+
     if Broadcast_IDs.get(broadcast_id):
+
         Broadcast_IDs.pop(broadcast_id)
+
     completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
+
     await asyncio.sleep(3)
+
     await out.delete()
+
     if failed == 0:
+
         await m.reply_text(
+
             text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+
             quote=True
+
         )
+
     else:
+
         await m.reply_document(
+
             document='broadcast.txt',
+
             caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+
             quote=True
+
         )
+
     os.remove('broadcast.txt')
