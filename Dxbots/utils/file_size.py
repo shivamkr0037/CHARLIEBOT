8f25e55d82c32d8e517e59dadffb48f8849f788e
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-25 21:18:23 +0530

    Update file_size.py

--- Dxbots/utils/file_size.py
+++ Dxbots/utils/file_size.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2024 DX-MODS
+#Licensed under the  MIT License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
 def human_size(bytes, units=[' bytes','KB','MB','GB','TB', 'PB', 'EB']):
     """ Returns a human readable string representation of bytes """
     return str(bytes) + units[0] if int(bytes) < 1024 else human_size(int(bytes)>>10, units[1:])
