513aa3d9e5cfda6b17f72bd3c8d82154e2f52313
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-25 21:19:05 +0530

    Update render_template.py

--- Dxbots/utils/render_template.py
+++ Dxbots/utils/render_template.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2024 DX-MODS
+#Licensed under the  MIT License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
 from Dxbots.vars import Var
 from Dxbots.bot import DxStreamBot
 from Dxbots.utils.human_readable import humanbytes
