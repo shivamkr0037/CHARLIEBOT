6aaef0708c42bbcb91297862a9b4a2ae1fbac3b5
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-25 21:17:44 +0530

    Update custom_dl.py

--- Dxbots/utils/custom_dl.py
+++ Dxbots/utils/custom_dl.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2024 DX-MODS
+#Licensed under the  MIT License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
 import math
 import asyncio
 import logging
