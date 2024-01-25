b7d53fd4dde2938a67a6d6fa20e3988263d5e175
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-25 21:17:23 +0530

    Update config_parser.py

--- Dxbots/utils/config_parser.py
+++ Dxbots/utils/config_parser.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2024 DX-MODS
+#Licensed under the  MIT License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
+
 from os import environ
 from typing import Dict, Optional
 
