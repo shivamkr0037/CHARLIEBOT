3b5e98a041e96c56364ddec40b9eeaee72d35b9f
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-25 21:05:50 +0530

    Update admin.py

--- Dxbots/bot/plugins/admin.py
+++ Dxbots/bot/plugins/admin.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2024 DX-MODS
-#Licensed under the  AGPL-3.0 License;
+#Licensed under the  MIT License;
 #you may not use this file except in compliance with the License.
 #Author ZIYAN
 #if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
