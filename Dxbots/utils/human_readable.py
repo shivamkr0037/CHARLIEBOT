54236f617501c3a2d974f4fec19442ecc6bd8cde
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-25 21:18:36 +0530

    Update human_readable.py

--- Dxbots/utils/human_readable.py
+++ Dxbots/utils/human_readable.py
@@ -1,4 +1,8 @@
-
+# Copyright (C) 2024 DX-MODS
+#Licensed under the  MIT License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
 
 def humanbytes(size):
     # https://stackoverflow.com/a/49361727/4723940
